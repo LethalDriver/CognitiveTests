e8407feb1c45dd1c2b7ca8552bafe6a6dc66d0fc
mwdziak <mwdziak@gmail.com>
2024-06-05 14:29:02 +0200

    Implemented filtering in scores fragment.

--- app/src/main/java/com/example/cognitivetests/ScoresFragment.kt
+++ app/src/main/java/com/example/cognitivetests/ScoresFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cognitivetests.recyclerView.Score
 import com.example.cognitivetests.recyclerView.ScoresAdapter
+import com.google.android.material.chip.Chip
 
 class ScoresFragment : Fragment() {
     override fun onCreateView(
@@ -23,11 +24,40 @@ class ScoresFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val scores = arrayOf(
-            Score("TMT", "2021-09-01", 10, null, null),
+            Score("Trail Making", "2021-09-01", 10, null, null),
             Score("Stroop", "2021-09-01", null, 3, 60),
         )
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = ScoresAdapter(scores)
+
+        val stroopChip = view.findViewById<Chip>(R.id.stroopChip)
+        val tmtChip = view.findViewById<Chip>(R.id.tmtChip)
+        val digitSubChip = view.findViewById<Chip>(R.id.digitSubChip)
+
+        val chips = arrayOf(stroopChip, tmtChip, digitSubChip)
+
+        val selectedTestTypes = mutableListOf<String>()
+
+        for (chip in chips) {
+            chip.isCheckable = true
+            chip.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    // The chip has been checked
+                    selectedTestTypes.add(chip.text.toString())
+                } else {
+                    // The chip has been unchecked
+                    selectedTestTypes.remove(chip.text.toString())
+                }
+
+                // Filter the scores based on the selected test types
+                val filteredScores = if (selectedTestTypes.isEmpty()) {
+                    scores
+                } else {
+                    scores.filter { it.testType in selectedTestTypes }.toTypedArray()
+                }
+                recyclerView.adapter = ScoresAdapter(filteredScores)
+            }
+        }
     }
 }
