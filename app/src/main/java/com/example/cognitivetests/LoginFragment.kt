39a42a651d17eaac1d143f78068221fa43055cfa
mwdziak <mwdziak@gmail.com>
2024-06-12 18:25:40 +0200

    Login fragment imported from earlier project.

---
+++ app/src/main/java/com/example/cognitivetests/LoginFragment.kt
@@ -0,0 +1,74 @@
+package com.example.cognitivetests
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LoginFragment : Fragment() {
+    private val viewModel: LoginViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        if (viewModel.isUserLoggedIn()) {
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val registerLink = view.findViewById<TextView>(R.id.registerLink)
+        val editTextEmail = view.findViewById<TextView>(R.id.editTextEmail)
+        val editTextPassword = view.findViewById<TextView>(R.id.editTextPassword)
+        val loginButton = view.findViewById<TextView>(R.id.loginButton)
+
+        editTextEmail.addTextChangedListener { text ->
+            viewModel.updateEmail(text.toString())
+        }
+
+        editTextPassword.addTextChangedListener { text ->
+            viewModel.updatePassword(text.toString())
+        }
+
+        loginButton.setOnClickListener {
+            viewModel.authenticate()
+        }
+
+        viewModel.authenticationState.observe(viewLifecycleOwner) { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.LOADING -> {
+                    loginButton.isEnabled = false
+                }
+
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    loginButton.isEnabled = true
+                    val intent = Intent(requireContext(), MainActivity::class.java)
+                    startActivity(intent)
+                    activity?.finish()
+                }
+
+                LoginViewModel.AuthenticationState.FAILED -> {
+                    loginButton.isEnabled = true
+                }
+                null -> {
+                    loginButton.isEnabled = true
+                }
+            }
+        }
+        viewModel.exceptionMessage.observe(viewLifecycleOwner) { message ->
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cognitivetests/LoginViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.cognitivetests
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cognitivetests.auth.AuthenticationRequest
+import com.example.cognitivetests.service.HttpService
+import com.example.cognitivetests.service.TokenManager
+import kotlinx.coroutines.launch
+
+class LoginViewModel(private val tokenManager: TokenManager, private val httpService: HttpService) : ViewModel() {
+    private val email = MutableLiveData<String>("")
+    private val password = MutableLiveData<String>("")
+    val exceptionMessage = MutableLiveData<String>()
+    val authenticationState = MutableLiveData<AuthenticationState>()
+    enum class AuthenticationState {
+        LOADING, AUTHENTICATED, FAILED
+    }
+    fun updateEmail(newEmail: String) {
+        email.value = newEmail
+    }
+
+    fun updatePassword(newPassword: String) {
+        password.value = newPassword
+    }
+
+    fun authenticate() {
+        viewModelScope.launch {
+            authenticationState.value = AuthenticationState.LOADING
+            try {
+                val authenticationRequest = AuthenticationRequest(email.value ?: "", password.value ?: "")
+                val tokensDTO = httpService.authenticate(authenticationRequest)
+                tokenManager.saveTokens(tokensDTO.token, tokensDTO.refreshToken, tokensDTO.expirationDate)
+                authenticationState.value = AuthenticationState.AUTHENTICATED
+            } catch (e: Exception) {
+                exceptionMessage.postValue(e.message)
+                authenticationState.value = AuthenticationState.FAILED
+            }
+        }
+    }
+
+    fun isUserLoggedIn(): Boolean {
+        return !tokenManager.isRefreshTokenExpired();
+    }
+
+}
